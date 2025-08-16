b13d9d11eb23a5198f87f27e001845cf82e34d83
Melvin J Bonner <140230755+jb24000@users.noreply.github.com>
2025-08-16 07:24:37 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'cli30-ux-v1';
+const CACHE = 'cli-mastery-awsx-v2';
 const FILES = ['./','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE && caches.delete(k))))));
